4c3ccd15809af8ca6ad57b46637d9aff085ec360
zeliran <456489+zeliran@users.noreply.github.com>
2021-05-27 12:52:43 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,60 +1,37 @@
-var font;
-var vehicles = [];
+var font
+var vehicles = []
 
 function preload() {
-  font = loadFont('AvenirNextLTPro-Demi.otf');
+  font = loadFont('AvenirNextLTPro-Demi.otf')
 }
 
 function setup() {
-  
-  createCanvas(600, 300);
-  background(51);
-  // textFont(font);
-  // textSize(192);
-  // fill(255);
-  // noStroke();
-  // text('train', 100, 200);
-  
-  var points = font.textToPoints('train long sent', 10, 200, 80, {sampleFactor:0.4});
-  
+  createCanvas(920, 250)
+  var points = font.textToPoints('Zorba Dance', 10, 170, 150, {
+    sampleFactor: 0.4,
+  })
+  var points2 = font.textToPoints('EZ', 400, 220, 80, {
+    sampleFactor: 0.4,
+  })
   for (var ii = 0; ii < points.length; ii++) {
-    var pt = points[ii];
-    var vehicle = new Vehicle(pt.x, pt.y);
-    vehicles.push(vehicle);
-    // stroke(255);
-    // strokeWeight(8);
-    // point(pt.x, pt.y);
+    var pt = points[ii]
+    let y = pt.y + 50 * cos((2 * PI * pt.x) / width)
+    var vehicle = new Vehicle(pt.x, y)
+    vehicles.push(vehicle)
+  }
+  for (var ii = 0; ii < points2.length; ii++) {
+    var pt = points2[ii]
+    var vehicle = new Vehicle(pt.x, pt.y)
+    vehicles.push(vehicle)
   }
 }
 
 function draw() {
-  background(51);
-/*   var yoff = 0;
-  for (var y = 0; y < rows; y++) {
-    var xoff = 0;
-    for (var x = 0; x < cols; x++) {
-      var index = x + y * cols;
-      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
-      var v = p5.Vector.fromAngle(angle);
-      v.setMag(1);
-      flowfield[index] = v;
-      xoff += inc;
-      stroke(0, 50);
-      // push();
-      // translate(x * scl, y * scl);
-      // rotate(v.heading());
-      // strokeWeight(1);
-      // line(0, 0, scl, 0);
-      // pop();
-    }
-    yoff += inc;
-
-    zoff += 0.0003;
-  }*/
-
+  // clear()
+  background(255, 50)
   for (var ii = 0; ii < vehicles.length; ii++) {
-    vehicles[ii].behaviors();
-    vehicles[ii].update();
-    vehicles[ii].show();
+    vehicles[ii].behaviors()
+    vehicles[ii].update()
+    vehicles[ii].show()
   }
 }
